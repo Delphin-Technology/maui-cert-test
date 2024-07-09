ef69538cd800a9b5da0672a384de71a8c0e68adb
Pascal Richter <Pascal.Richter@delphin.de>
2024-07-09 10:13:04 +0200

    Initial code commit

---
+++ MauiCertTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace MauiCertTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void FetchSelfSignedUrlClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+                };
+                var httpClient = new HttpClient(handler);
+                var response = await httpClient.GetAsync("https://self-signed.badssl.com");
+                FetchResult.Text = response.StatusCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                FetchResult.Text = ex.ToString();
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+
+}
